004fe1e379d02c0e0abb96805ddc36bb6666e11b
AlejoPaglino <136344335+AlejoPaglino@users.noreply.github.com>
2023-08-11 09:00:19 -0300

    Add files via upload

---
+++ alejo/alejo/clase1.js
@@ -0,0 +1,28 @@
+alert('hola')
+let suma = 10 + 10
+let resta = 70 - 10
+let multiplicacion = 10 * 10
+let divicion = 10 / 2
+console .log('El resultado de la suma es ' + suma)
+console .log('el resultado es ' + resta)
+console .log('el resultado es ' + multiplicacion)
+console .log('el resultado es ' + divicion)
+let nombreAlumno
+ nombreAlumno = prompt('ingresa tu nombre')
+ alert ( 'bienvenido ' +  nombreAlumno)
+ let edadAlumno
+ edadAlumno = prompt('ingresa tu edad ')
+ alert ('tengo' + edadAlumno + ' años')
+let añoNacimiento
+añoNacimiento = prompt('ingresa tu año de nacimiento')
+alert ('mi fecha de nacimiento es en ' + añoNacimiento)
+let nombreCompañero
+nombreCompañero = prompt('¿como te llamas?')
+alert('bienvenido al ivt ' + nombreCompañero)
+let nombreAlumno2 = ' Alejo '
+let apellidoAlumno = ' Paglino '
+console.log ( 'bienvenido' + nombreAlumno2 + apellidoAlumno)
+let Diez = 10
+let tres = 3
+let resultadomultiplicacion = (Diez * tres)
+alert ('El resultado de la multiplicaciopn es ' + resultadomultiplicacion)
